17b33ba7f516f56bf748333d29f209b6c196e124
Jins Babu <jinsbabu03@gmail.com>
2026-01-29 01:20:10 -0800

    Add files via upload

---
+++ matrixaddition.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+class MatrixAddition
+{
+    public static void main(String[] args) {
+        Scanner sc=new 
+        Scanner(System.in);
+   System.out.print("Enter number of rows:");
+   int rows=sc.nextInt();
+   System.out.print("Enter number of columns:");
+   int cols=sc.nextInt();
+   int[][]A=new int[rows][cols];
+   int[][]B=new int[rows][cols];
+   int[][]sum=new int[rows][cols];
+   System.out.print("Enter elements of first matrix:");
+for(int i=0;i<rows;i++)
+   {
+    for(int j=0;j<cols;j++)
+    {
+        A[i][j]=sc.nextInt();
+    }
+   }
+  System.out.print("Enter elements of second matrix:");
+for(int i=0;i<rows;i++)
+   {
+    for(int j=0;j<cols;j++)
+    {
+        B[i][j]=sc.nextInt();
+    }
+   }
+
+
+
+   for(int i=0;i<rows;i++)
+   {
+    for(int j=0;j<cols;j++)
+    {
+        sum[i][j]=A[i][j]+B[i][j];
+    }
+   }
+   System.out.print("sum of the matrices:");
+   for(int i=0;i<rows;i++)
+   {
+    for(int j=0;j<cols; j++) 
+        {
+        System.out.print(sum[i][j]+" ");
+
+    }
+System.out.println();
+}
+sc.close();
+    }
+}
